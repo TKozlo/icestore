91e7f863415950b0d0fb0942706fd84d555d778f
TKozlo <121772671+TKozlo@users.noreply.github.com>
2023-02-10 17:27:04 +0300

    change static root
    change static root/dir in Debug

--- store/settings.py
+++ store/settings.py
@@ -182,8 +182,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 MEDIA_URL = '/media/'
